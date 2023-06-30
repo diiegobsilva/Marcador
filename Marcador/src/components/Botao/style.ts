98451ebb68fe545502a01176621c62c06948bfad
diiegobsilva <diiegobsilva>
2023-06-30 16:11:31 -0300

    Botão

---
+++ Marcador/src/components/Botao/style.ts
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  textButton : {
+    color: "#fff",
+    fontSize: 35,
+    fontWeight: "bold"
+  },
+  textButtonZerar : {
+    color: "#fff",
+    fontSize: 13,
+    fontWeight: "bold"
+  },
+  buttonTruco : {
+    margin: 30,
+    borderRadius: 13,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "60%",
+    height: 50,
+    backgroundColor: "#A80E0E",
+  },
+  buttoZerar : {
+    borderRadius: 8,
+    margin: 20,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "27%",
+    height: 30,
+    backgroundColor: "#A80E0E",
+  },
+  });
+   
+  export default styles
--- Marcador/src/components/Main/index.tsx
+++ Marcador/src/components/Main/index.tsx
@@ -3,12 +3,14 @@ import {View, ImageBackground} from "react-native"
 import styles from "./style"
 
 import Titulo from "../Titulo";
+import Botao from "../Botao";
 
 export default function Main(){
     return(
         <View style={styles.boxTitle}>
             <Titulo titulo={"Eles"}/>
             <Titulo titulo={"Nós"}/>
+            <Botao/>
         </View>
     )
 }
