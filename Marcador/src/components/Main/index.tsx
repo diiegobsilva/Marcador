e00bd19ac54c05fb7266e8c3ef79d9de3384faec
diiegobsilva <diiegobsilva>
2023-06-30 15:25:22 -0300

    Background

--- Marcador/src/components/Main/index.tsx
+++ Marcador/src/components/Main/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import {View, Text} from "react-native"
+import {View, ImageBackground} from "react-native"
 import styles from "./style"
 
 export default function Main(){
     return(
         <View style={styles.boxTitle}>
-          
+          <ImageBackground source={require('../../assets/planoFundo.png')}  />
         </View>
     )
 }
