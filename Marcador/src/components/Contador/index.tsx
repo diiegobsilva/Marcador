8a43649829b650913ae3662a76e3acd952838a7f
mateushlsilva <mateushls01@gmail.com>
2023-07-01 14:50:53 -0300

    Adiciona logica contador

--- Marcador/src/components/Contador/index.tsx
+++ Marcador/src/components/Contador/index.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Image, Text, TouchableOpacity, View } from "react-native";
 import styles from "./style";
 
 export default function Contador({valor}:any) {
+
+    const [ pontos, setPontos ] = useState(0)
+
+    const alterarValor = (sinal:string) => {
+        if(sinal === "-" && pontos != 0){
+            let valor = pontos - 1  
+            setPontos(valor)
+        }
+        else if(sinal === "+"){
+            let valor = pontos + 1  
+            setPontos(valor)
+        }
+        
+    }
+
     return (
 
         <View style={styles.viewEsquerda}>
-            <Text style={styles.text}>{valor}</Text>
+            <Text style={styles.text}>{pontos}</Text>
           <View style={styles.coluna}>
-            <TouchableOpacity onPress={() => {console.log("oi")}}>
+            <TouchableOpacity onPress={() => alterarValor("-")}>
                 <Image style={styles.naipeOuros} source={require("../../assets/ouros.png")} />
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => {console.log("oi")}}>
+            <TouchableOpacity onPress={() => alterarValor("+")}>
                 <Image style={styles.naipeEspada} source={require("../../assets/espada.png")} />
             </TouchableOpacity>
           </View>
